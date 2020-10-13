5ecb8e1e80afc3b7d124b9d2e8c48f204e3cb071
Kristen Steele <ksteelephoto@gmail.com>
2020-10-12 19:04:31 -0500

    Add journal entry html export component

---
+++ scripts/JournalEntry.js
@@ -0,0 +1,13 @@
+/*
+ *  Purpose: To render a single journal entry as an
+ *           HTML representation of the data
+ */
+export const JournalEntryComponent = (entry) => {
+    return `
+        <section id="entry--${entry.id}" class="journalEntry">
+            <h3>${entry.concept}</h3>
+            <p>${entry.entry}</p>
+            <p>${entry.concept}</p>
+        </section>
+    `
+}
