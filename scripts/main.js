89bb480c16baa590e09a8319c9738e6eb9259404
Kristen Steele <ksteelephoto@gmail.com>
2020-10-23 16:13:50 -0500

    test getEntries

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { useJournalEntries } from "./JournalDataProvider.js"
+import { getEntries, useJournalEntries } from "./JournalDataProvider.js"
 import { EntryListComponent } from "./JournalEntryList.js"
 
 console.log("Welcome to the main module")
@@ -6,3 +6,4 @@ console.log("Welcome to the main module")
 // useJournalEntries()
 
 EntryListComponent()
+getEntries()
