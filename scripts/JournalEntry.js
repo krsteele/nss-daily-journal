aa45b4f50f72589c034219ad56590179c452303b
Kristen Steele <ksteelephoto@gmail.com>
2020-10-24 20:36:17 -0500

    add mood to entry html and reorder elements

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -4,10 +4,11 @@
  */
 export const JournalEntryComponent = (entry) => {
     return `
-        <section id="entry--${entry.id}" class="journalEntry">
+        <div id="entry--${entry.id}" class="journalEntry">
             <h3>${entry.concept}</h3>
-            <p>${entry.entry}</p>
             <p>${entry.date}</p>
-        </section>
+            <p>${entry.mood}</p>
+            <p>${entry.entry}</p>
+        </div>
     `
 }
