704c1cb8e21127b661f566e486528fa8ab28c261
Kristen Steele <ksteelephoto@gmail.com>
2020-10-25 11:53:08 -0500

    add event listener to refresh journal entry list when new entry is added. test and debug.

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -4,6 +4,12 @@ import { JournalEntryComponent } from "./JournalEntry.js"
 // DOM reference to where all entries will be rendered
 const contentTarget = document.querySelector(".entryContainer")
 
+// reference to eventHub
+const eventHub = document.querySelector("#container")
+
+// listen for state change event, then refresh entry list
+eventHub.addEventListener("entryStateChanged", () => EntryListComponent())
+
 export const EntryListComponent = () => {
     // use journal entry data from the data provider component
     getEntries()
@@ -12,8 +18,6 @@ export const EntryListComponent = () => {
         renderEntries(entries) 
     })
 }
-    
-
     
 const renderEntries = (array) => {
     let journalHTMLRepresentations = ""
