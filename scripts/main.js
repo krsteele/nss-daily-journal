5fb8e00c94817155f91519ec0ba541e53e9731cf
Kristen Steele <ksteelephoto@gmail.com>
2020-11-12 15:37:44 -0600

    refactor to call getMoods and .then call JournalForm and FilterBar

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+import { FilterBar } from "./FilterBar.js"
 import { getEntries, useJournalEntries } from "./JournalDataProvider.js"
 import { EntryListComponent } from "./JournalEntryList.js"
 import { JournalForm } from "./JournalForm.js"
+import { getMoods } from "./MoodProvider.js"
 
 console.log("Welcome to the main module")
 
@@ -9,4 +11,9 @@ console.log("Welcome to the main module")
 EntryListComponent()
 getEntries()
 
-JournalForm()  
+getMoods()
+    .then(() => {
+        JournalForm()
+        FilterBar()
+    }) 
+   
