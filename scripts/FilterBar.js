fd7930350ce085d694e0296ddfd7c094c85f90e3
Kristen Steele <ksteelephoto@gmail.com>
2020-11-12 13:43:20 -0600

    add FilterBar function to render mood filter to DOM

---
+++ scripts/FilterBar.js
@@ -0,0 +1,9 @@
+const contentTarget = document.querySelector(".entries__filters")
+
+<fieldset>
+    <legend>Show all entries for this mood...</legend>
+    <div>
+        <input type="radio" id="moodFilter" name="" value="">
+        <label for="moodFilter">${mood.name}</label>
+    </div>
+</fieldset>
