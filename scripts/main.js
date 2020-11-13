1e2ae4b7956a74daedd9cdf12c49f23496e26461
Kristen Steele <ksteelephoto@gmail.com>
2020-11-12 19:58:48 -0600

    import FilterBar.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { getEntries, useJournalEntries } from "./JournalDataProvider.js"
 import { EntryListComponent } from "./JournalEntryList.js"
 import { JournalForm } from "./JournalForm.js"
 import { getMoods } from "./MoodProvider.js"
+import "./FilterBar.js"
 
 console.log("Welcome to the main module")
 
