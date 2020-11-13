2cef4d098380b02e90e8f67547b99fef2616373c
Kristen Steele <ksteelephoto@gmail.com>
2020-11-12 19:57:48 -0600

    add event listener for mood radio change and dispatch event with detail

--- scripts/FilterBar.js
+++ scripts/FilterBar.js
@@ -2,6 +2,7 @@ import { useMoods } from "./MoodProvider.js"
 import { MoodFilter } from "./MoodFilter.js"
 
 const contentTarget = document.querySelector(".entries__filters")
+const eventHub = document.querySelector("#container")
 
 export const FilterBar = () => {
     const moods = useMoods()
@@ -14,3 +15,21 @@ const render = moodArray => {
         ${MoodFilter(moodArray)}
         `
 }
+
+eventHub.addEventListener("change", event => {
+    // console.log("I hear a thing")
+    if (event.target.name === "moodFilter") {
+        // console.log("you have chosen a mood")
+        // console.log("mood chosen event:", event.target.value)
+        const moodChosen = parseInt(event.target.value)
+        const customEvent = new CustomEvent("moodFilterChosen", {
+            detail: {
+                mood: moodChosen
+    }
+})
+
+console.log(customEvent)
+eventHub.dispatchEvent(customEvent)
+    }
+
+})
